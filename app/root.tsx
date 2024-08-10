65440e6da1f80ccb51a006432603cc8ac6c9ddf7
suhas-sunder <suhas@live.ca>
2024-08-10 00:40:48 -0400

    feat: google analytics
    -Clean up root file and configure Google Analytics.

--- app/root.tsx
+++ app/root.tsx
@@ -4,8 +4,6 @@ import Footer from "./client/components/navigation/Footer";
 import ReactGA from "react-ga4";
 
 import {
-  ClientLoaderFunctionArgs,
-  json,
   Outlet,
   Links,
   Meta,
@@ -13,101 +11,7 @@ import {
   ScrollRestoration,
   useLocation,
 } from "@remix-run/react";
-import localforage from "localforage";
-import cloudflareR2API from "./client/components/api/cloudflareR2API";
 import { useEffect, useState } from "react";
-import pako from "pako";
-import { Filename } from "./routes/_index";
-
-// Loader function to fetch and decompress the data
-export const loader = async () => {
-  let filenames = [];
-
-  try {
-    const response = await cloudflareR2API.get("/emojis/filenames.json.gz", {
-      method: "GET",
-      responseType: "arraybuffer",
-    });
-
-    const isGzip = response.data[0] === 0x1f && response.data[1] === 0x8b;
-
-    if (isGzip) {
-      const decompressedData = pako.ungzip(new Uint8Array(response.data), {
-        to: "string",
-      });
-      filenames = JSON.parse(decompressedData);
-    } else {
-      const textData = new TextDecoder().decode(response.data);
-      filenames = JSON.parse(textData);
-    }
-  } catch (err) {
-    console.error("Failed to fetch or decompress filenames for emoji!", err);
-  }
-
-  return json(
-    { filenames },
-    {
-      headers: {
-        "Cache-Control": "max-age=3600, public",
-      },
-    }
-  );
-};
-
-// Client Loader Function to handle caching
-export async function clientLoader({ serverLoader }: ClientLoaderFunctionArgs) {
-  const cacheKey = "filenames";
-
-  try {
-    const cachedFilenames = await localforage.getItem(cacheKey);
-
-    if (cachedFilenames) {
-      return { filenames: cachedFilenames };
-    } else {
-      const { filenames }: { filenames: Filename[] } = await serverLoader();
-      await localforage.setItem(cacheKey, filenames);
-
-      //Cache each emoji image from the server
-      const fetchAndStoreImage = async (url: string) => {
-        // Check if the URL is valid and if the image blob is already in the state
-        if (!url) {
-          return;
-        }
-
-        try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            console.error(
-              `Failed to fetch image from URL: ${url}, Status: ${response.status}`
-            );
-            return;
-          }
-          const blob = await response.blob();
-          if (blob) {
-            await localforage.setItem(url, blob);
-          } else {
-            console.error(`No blob received for URL: ${url}`);
-          }
-        } catch (error) {
-          console.error(`Error fetching image from URL: ${url}`, error);
-        }
-      };
-
-      filenames.forEach((filename) => {
-        fetchAndStoreImage(
-          `https://www.honeycombartist.com/emojis/base/${filename.id.slice(
-            1
-          )}.png`
-        );
-      });
-
-      return { filenames };
-    }
-  } catch (error) {
-    console.error("Error fetching cached filenames:", error);
-    return { filenames: [] };
-  }
-}
 
 // Layout Component for rendering HTML structure
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -132,15 +36,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 // App Component for managing application state
 export default function App() {
-  const [copyText, setCopyText] = useState("");
-  const [displayCopyText, setDisplayCopyText] = useState("");
-  const [textareaIsHidden, setTextareaIsHidden] = useState(false);
   const [stopGADelayOnStart, setStopGADelayOnStart] = useState(false);
   const pathname = useLocation().pathname;
 
   useEffect(() => {
     const loadGoogleAnalyticsAdsense = async () => {
-      await ReactGA.initialize("G-MZ3BW959JC"); // Initialize Google Analytics
+      await ReactGA.initialize("G-9C6MJ7Y439"); // Initialize Google Analytics
 
       // Send page view with a custom path
       ReactGA.send({
@@ -161,38 +62,5 @@ export default function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname]);
 
-  useEffect(() => {
-    if (copyText) {
-      // Clean the text by removing any line breaks or unnecessary spaces
-      const cleanedText = copyText.replace(/\s*\n\s*/g, "");
-
-      // Copy the cleaned text to the clipboard
-      navigator.clipboard
-        .writeText(cleanedText)
-        .then(() => {
-          // Update the display text
-          setDisplayCopyText(
-            (prevDisplayCopyText) => `${prevDisplayCopyText} ${cleanedText}`
-          );
-          // Clear the copyText state
-          setCopyText("");
-        })
-        .catch((err) => {
-          console.error("Failed to copy text: ", err);
-        });
-    }
-  }, [copyText]);
-
-  return (
-    <Outlet
-      context={{
-        copyText,
-        setCopyText,
-        displayCopyText,
-        setDisplayCopyText,
-        textareaIsHidden,
-        setTextareaIsHidden,
-      }}
-    />
-  );
+  return <Outlet />;
 }
