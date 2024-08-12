605f71c6255f6f628731c15157421b6249dd516c
suhas-sunder <suhas@live.ca>
2024-08-12 01:57:36 -0400

    feat: main menu & game logic
    -Add main menu on home page with skull animation and play button
    
    -Add a page for each game difficulty level and add logic to play button to redirect to the medium difficulty page by default.
    
    Add skulls array to a separate component exported for reusability.
    
    -Add logic to medium difficulty games page to handle keypress logic so that the characters typed get added to each square. -Add logic to handle character deletion. -Add logic to stop squares from being updated if the row is complete. -If a row is completed, and the user presses enter, switch to the next row.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,198 +1,65 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useNavigate } from "@remix-run/react";
+import { useState, useEffect, useMemo } from "react";
 import { v4 as uuidv4 } from "uuid";
+import Skulls from "../client/components/data/skulls";
 
-export const meta: MetaFunction = () => {
-  return [
-    {
-      title:
-        "💀 Word Skull - An educational word puzzle game designed to look like skulls and other fun shapes... 🎉✨",
-    },
-    {
-      name: "description",
-      content:
-        "Are you worthy? Find out by playing this fun word guessing game! Challenge your mind, and improve your vocabulary in no time! 🎉📲",
-    },
-  ];
+const getRandomTransform = () => {
+  const scale = Math.random() * (1 - 0.6) + 0.6; // Random scale between 0.7 and 1.1
+  const rotate = Math.floor(Math.random() * 360); // Random rotation between 0 and 360 degrees
+  const translateX = Math.random() * 60 - 30; // Random horizontal translation
+  const translateY = Math.random() * 60 - 30; // Random vertical translation
+
+  return {
+    scale: `scale(${scale.toFixed(2)})`,
+    rotate: `rotate(${rotate}deg)`,
+    translateX: `translateX(${Math.round(translateX)}px)`,
+    translateY: `translateY(${Math.round(translateY)}px)`,
+  };
+};
+
+// Function to shuffle an array
+const shuffleArray = (array: string[][][]) => {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
 };
 
 export default function Index() {
-  //Easy Skull
-  const skull_1 = [
-    ["", "", "", ""],
-    ["", "@", "", "", "@", ""],
-    ["", "@", "@", "", "@", "@", ""],
-    ["", "", "", "", ""],
-    ["", "", "", ""],
-    ["", "", "", ""],
-  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [transformStyle, setTransformStyle] = useState({});
+  const [fadeClass, setFadeClass] = useState("fade-in"); // State to manage fade class
+  const navigate = useNavigate();
 
-  const skull_2 = [
-    ["", "", "", ""],
-    ["", "@", "", "", "", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "", "~", "", "@", ""],
-    ["", "", "", "", ""],
-    ["", "", "", ""],
-  ];
-  const skull_3 = [
-    ["~", "", "", "~", "~", "", "", "~"],
-    ["", "@", "", "@", "", "@", "", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "", "~", "~", "~", "", ""],
-    ["", "", "~", "", ""],
-    ["", "", "", ""],
-  ];
+  const skulls = useMemo(() => Skulls(), []);
 
-  const skull_4 = [
-    ["~", "", "", "~", "~", "", "", "~"],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "", "@", "", "@", "", "@", ""],
-    ["~", "", "", "~", "~", "", "", "~"],
-    ["", "", "~", "", ""],
-    ["", "", "", ""],
-  ];
+  // Shuffle skulls initially
+  const [shuffledSkulls] = useState(shuffleArray(skulls));
 
-  // Medium Skull
-  const skull_5 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "@", "@", "", "", "@", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "@", "", "", "@", "@", ""],
-    ["", "", "", "~", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const fadeOutTransform = getRandomTransform();
+      setFadeClass("fade-out"); // Start fade-out
+      setTransformStyle({
+        transform: `${fadeOutTransform.scale} ${fadeOutTransform.rotate} ${fadeOutTransform.translateX} ${fadeOutTransform.translateY}`,
+        opacity: 0, // Fade out to opacity 0
+      });
 
-  const skull_6 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "@", "", "", "", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "", "~", "", "@", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-  const skull_7 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["~", "", "", "", "", "", "", "~"],
-    ["", "@", "", "@", "", "@", "", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "", "", "~", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % shuffledSkulls.length);
+        const fadeInTransform = getRandomTransform(); // Get new transform for fade-in
+        setTransformStyle({
+          transform: `${fadeInTransform.scale} ${fadeInTransform.rotate} ${fadeInTransform.translateX} ${fadeInTransform.translateY}`,
+          opacity: 1, // Fade in to opacity 1
+        });
+        setFadeClass("fade-in"); // Start fade-in
+      }, 2000); // Duration to match fade-out
+    }, 5000); // Duration to show each skull
 
-  const skull_8 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["~", "", "", "", "", "", "", "~"],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "", "@", "", "@", "", "@", ""],
-    ["", "", "", "~", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-
-  //Hard SKull
-  const skull_9 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "@", "", "", "", "", "@", ""],
-    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
-    ["", "@", "", "", "", "", "@", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-
-  const skull_10 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "@", "@", "", "", "", "@", "@", ""],
-    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
-    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
-    ["", "@", "", "", "", "", "@", ""],
-    ["", "", "", "", "~", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "", "~", ""],
-  ];
-
-  const skull_11 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "", "", "~", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-
-  const skull_12 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "@", "@", "", "@", "@", "", ""],
-    ["", "", "@", "@", "", "@", "@", "", ""],
-    ["", "", "", "~", "~", "", "", ""],
-    ["", "", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-
-  //Extreme SKull
-  const skull_13 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "@", "", "", "", "", "@", "", ""],
-    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
-    ["", "", "@", "", "", "", "", "@", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-
-  const skull_14 = [
-    ["", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "@", "@", "", "", "", "@", "@", ""],
-    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
-    ["", "", "@", "@", "", "", "", "@", "@", "", ""],
-    ["", "", "", "", "~", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "", "~", ""],
-  ];
-
-  const skull_15 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "", "", "~", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
-
-  const skull_16 = [
-    ["", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "@", "@", "@", "", "@", "@", "@", ""],
-    ["", "", "", "", "", "", "", "", ""],
-    ["", "", "", "~", "", "", ""],
-    ["", "", "", "", "", ""],
-    ["", "~", "", "~", ""],
-  ];
+    return () => clearInterval(interval);
+  }, [shuffledSkulls.length]);
 
   const handleListItem = (square: string, squareCount: number) => {
     if (square === "@")
@@ -225,24 +92,32 @@ export default function Index() {
   };
 
   return (
-    <>
-      <header className=" mb-10">
-        <h1 className="w-full flex justify-center items-center text-6xl text-center mt-10 leading-snug -translate-y-[0.3em] sm:translate-y-0 sm:mt-5 sm:mb-1 text-slate-500 font-lora">
-          W💀RD SKULL
+    <div className="flex  flex-col overflow-hidden">
+      <header>
+        <h1 className="w-full flex-col z-10 sm:flex-row flex justify-center gap-2 sm:gap-5 items-center text-6xl text-center mt-10 leading-snug -translate-y-[0.3em] sm:translate-y-0 sm:mt-5 sm:mb-1 text-slate-500 font-lora animate-fadeIn">
+          <span className="whitespace-nowrap">
+            <span className="inline-flex">W</span>
+            <span className="inline-flex animate-scalePulse">💀</span>
+            <span className="inline-flex">RD</span>
+          </span>
+          <span className="inline-flex">SKULL</span>
         </h1>
       </header>
-      <main className="flex justify-center flex-col pt-10 gap-14 items-center">
-        {[skull_1, skull_2, skull_3, skull_4].map((skull, index) => (
+      <main className="flex flex-col-reverse sm:flex-col sm:gap-14 items-center">
+        <div className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[28em] sm:min-h-[40em]">
           <div
-            key={index}
-            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
+            className={`absolute top-0 left-0  w-full h-full   flex flex-col justify-center items-center transition-opacity duration-2000 ease-in-out ${fadeClass}`}
+            style={{
+              ...transformStyle,
+              transition: "opacity 2s ease-in-out, transform 2s ease-in-out",
+            }}
           >
-            {skull.map((row) => {
+            {shuffledSkulls[currentIndex].map((row: string[]) => {
               let squareCount = 0; // Reset squareCount at the start of each row
 
               return (
                 <ul key={uuidv4()} className="flex">
-                  {row.map((square) => {
+                  {row.map((square: string) => {
                     if (square !== "@" && square !== "~") {
                       squareCount += 1; // Increment squareCount only for empty squares
                     }
@@ -252,71 +127,15 @@ export default function Index() {
               );
             })}
           </div>
-        ))}
-        {[skull_5, skull_6, skull_7, skull_8].map((skull, index) => (
-          <div
-            key={index}
-            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
-          >
-            {skull.map((row) => {
-              let squareCount = 0; // Reset squareCount at the start of each row
+        </div>
 
-              return (
-                <ul key={uuidv4()} className="flex">
-                  {row.map((square) => {
-                    if (square !== "@" && square !== "~") {
-                      squareCount += 1; // Increment squareCount only for empty squares
-                    }
-                    return handleListItem(square, squareCount);
-                  })}
-                </ul>
-              );
-            })}
-          </div>
-        ))}
-        {[skull_9, skull_10, skull_11, skull_12].map((skull, index) => (
-          <div
-            key={index}
-            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
-          >
-            {skull.map((row) => {
-              let squareCount = 0; // Reset squareCount at the start of each row
-
-              return (
-                <ul key={uuidv4()} className="flex">
-                  {row.map((square) => {
-                    if (square !== "@" && square !== "~") {
-                      squareCount += 1; // Increment squareCount only for empty squares
-                    }
-                    return handleListItem(square, squareCount);
-                  })}
-                </ul>
-              );
-            })}
-          </div>
-        ))}
-        {[skull_13, skull_14, skull_15, skull_16].map((skull, index) => (
-          <div
-            key={index}
-            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
-          >
-            {skull.map((row) => {
-              let squareCount = 0; // Reset squareCount at the start of each row
-
-              return (
-                <ul key={uuidv4()} className="flex">
-                  {row.map((square) => {
-                    if (square !== "@" && square !== "~") {
-                      squareCount += 1; // Increment squareCount only for empty squares
-                    }
-                    return handleListItem(square, squareCount);
-                  })}
-                </ul>
-              );
-            })}
-          </div>
-        ))}
+        <button
+          onClick={() => navigate("/word-skull-game-medium-mode")}
+          className="flex z-10 border-2 px-8 py-1 text-2xl font-nunito rounded-lg tracking-widest leading-loose text-slate-500 hover:text-skull-dark-brown hover:border-skull-brown"
+        >
+          Play
+        </button>
       </main>
-    </>
+    </div>
   );
 }
---
+++ app/routes/temp_1.tsx
@@ -0,0 +1,322 @@
+import type { MetaFunction } from "@remix-run/node";
+import { v4 as uuidv4 } from "uuid";
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title:
+        "💀 Word Skull - An educational word puzzle game designed to look like skulls and other fun shapes... 🎉✨",
+    },
+    {
+      name: "description",
+      content:
+        "Are you worthy? Find out by playing this fun word guessing game! Challenge your mind, and improve your vocabulary in no time! 🎉📲",
+    },
+  ];
+};
+
+export default function Index() {
+  //Easy Skull
+  const skull_1 = [
+    ["", "", "", ""],
+    ["", "@", "", "", "@", ""],
+    ["", "@", "@", "", "@", "@", ""],
+    ["", "", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+  ];
+
+  const skull_2 = [
+    ["", "", "", ""],
+    ["", "@", "", "", "", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "", "~", "", "@", ""],
+    ["", "", "", "", ""],
+    ["", "", "", ""],
+  ];
+  const skull_3 = [
+    ["~", "", "", "~", "~", "", "", "~"],
+    ["", "@", "", "@", "", "@", "", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "", "~", "~", "~", "", ""],
+    ["", "", "~", "", ""],
+    ["", "", "", ""],
+  ];
+
+  const skull_4 = [
+    ["~", "", "", "~", "~", "", "", "~"],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "", "@", "", "@", "", "@", ""],
+    ["~", "", "", "~", "~", "", "", "~"],
+    ["", "", "~", "", ""],
+    ["", "", "", ""],
+  ];
+
+  // Medium Skull
+  const skull_5 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "@", "@", "", "", "@", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "@", "", "", "@", "@", ""],
+    ["", "", "", "~", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const skull_6 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "@", "", "", "", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "", "~", "", "@", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+  const skull_7 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["~", "", "", "", "", "", "", "~"],
+    ["", "@", "", "@", "", "@", "", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "", "", "~", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const skull_8 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["~", "", "", "", "", "", "", "~"],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "", "@", "", "@", "", "@", ""],
+    ["", "", "", "~", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  //Hard SKull
+  const skull_9 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "@", "", "", "", "", "@", ""],
+    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
+    ["", "@", "", "", "", "", "@", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const skull_10 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "@", "@", "", "", "", "@", "@", ""],
+    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
+    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
+    ["", "@", "", "", "", "", "@", ""],
+    ["", "", "", "", "~", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "", "~", ""],
+  ];
+
+  const skull_11 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "", "", "~", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const skull_12 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "@", "@", "", "@", "@", "", ""],
+    ["", "", "@", "@", "", "@", "@", "", ""],
+    ["", "", "", "~", "~", "", "", ""],
+    ["", "", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  //Extreme SKull
+  const skull_13 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "@", "", "", "", "", "@", "", ""],
+    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
+    ["", "", "@", "", "", "", "", "@", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const skull_14 = [
+    ["", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "@", "@", "", "", "", "@", "@", ""],
+    ["", "@", "@", "@", "", "", "@", "@", "@", ""],
+    ["", "", "@", "@", "", "", "", "@", "@", "", ""],
+    ["", "", "", "", "~", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "", "~", ""],
+  ];
+
+  const skull_15 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "", "", "~", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const skull_16 = [
+    ["", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "@", "@", "@", "", "@", "@", "@", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "~", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "~", "", "~", ""],
+  ];
+
+  const handleListItem = (square: string, squareCount: number) => {
+    if (square === "@")
+      return (
+        <li
+          key={uuidv4()}
+          className="text-[1.1rem] sm:text-[2rem] border-2 border-slate-700 bg-slate-800 rounded-lg w-[2em] h-[2em] flex justify-center items-center"
+        ></li>
+      );
+
+    if (square === "~")
+      return (
+        <li
+          key={uuidv4()}
+          className="text-[1.1rem] sm:text-[2rem] border-2 rounded-lg w-[2em] h-[2em] flex justify-center items-center"
+        ></li>
+      );
+
+    return (
+      <li
+        key={uuidv4()}
+        className="text-[1.1rem] relative sm:text-[2rem] border-2 border-slate-400 rounded-lg w-[2em] h-[2em] flex justify-center items-center"
+      >
+        <span className="absolute text-sm text-slate-300 flex top-[0.02em] left-[0.3em]">
+          {squareCount}
+        </span>
+        <span>{square}</span>
+      </li>
+    );
+  };
+
+  return (
+    <>
+      <header className=" mb-10">
+        <h1 className="w-full flex justify-center items-center text-6xl text-center mt-10 leading-snug -translate-y-[0.3em] sm:translate-y-0 sm:mt-5 sm:mb-1 text-slate-500 font-lora">
+          W💀RD SKULL
+        </h1>
+      </header>
+      <main className="flex justify-center flex-col pt-10 gap-14 items-center">
+        {[skull_1, skull_2, skull_3, skull_4].map((skull, index) => (
+          <div
+            key={index}
+            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
+          >
+            {skull.map((row) => {
+              let squareCount = 0; // Reset squareCount at the start of each row
+
+              return (
+                <ul key={uuidv4()} className="flex">
+                  {row.map((square) => {
+                    if (square !== "@" && square !== "~") {
+                      squareCount += 1; // Increment squareCount only for empty squares
+                    }
+                    return handleListItem(square, squareCount);
+                  })}
+                </ul>
+              );
+            })}
+          </div>
+        ))}
+        {[skull_5, skull_6, skull_7, skull_8].map((skull, index) => (
+          <div
+            key={index}
+            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
+          >
+            {skull.map((row) => {
+              let squareCount = 0; // Reset squareCount at the start of each row
+
+              return (
+                <ul key={uuidv4()} className="flex">
+                  {row.map((square) => {
+                    if (square !== "@" && square !== "~") {
+                      squareCount += 1; // Increment squareCount only for empty squares
+                    }
+                    return handleListItem(square, squareCount);
+                  })}
+                </ul>
+              );
+            })}
+          </div>
+        ))}
+        {[skull_9, skull_10, skull_11, skull_12].map((skull, index) => (
+          <div
+            key={index}
+            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
+          >
+            {skull.map((row) => {
+              let squareCount = 0; // Reset squareCount at the start of each row
+
+              return (
+                <ul key={uuidv4()} className="flex">
+                  {row.map((square) => {
+                    if (square !== "@" && square !== "~") {
+                      squareCount += 1; // Increment squareCount only for empty squares
+                    }
+                    return handleListItem(square, squareCount);
+                  })}
+                </ul>
+              );
+            })}
+          </div>
+        ))}
+        {[skull_13, skull_14, skull_15, skull_16].map((skull, index) => (
+          <div
+            key={index}
+            className="relative flex-col w-full max-w-[800px] capitalize flex font-nunito text-slate-400 items-center min-h-[40em]"
+          >
+            {skull.map((row) => {
+              let squareCount = 0; // Reset squareCount at the start of each row
+
+              return (
+                <ul key={uuidv4()} className="flex">
+                  {row.map((square) => {
+                    if (square !== "@" && square !== "~") {
+                      squareCount += 1; // Increment squareCount only for empty squares
+                    }
+                    return handleListItem(square, squareCount);
+                  })}
+                </ul>
+              );
+            })}
+          </div>
+        ))}
+      </main>
+    </>
+  );
+}
