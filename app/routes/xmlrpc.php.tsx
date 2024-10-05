645e9ca82f27dd79cece18ee2a7914f204da8820
Suhas Sunder <suhas@live.ca>
2024-10-05 00:32:53 -0400

    refactor: wildcard
    -Refactor wildcard page to handle unwanted requests in action & loader.
    
    -Add placeholder ErrorBoundary to display 404 warning on client side.

--- app/routes/xmlrpc.php.tsx
+++
@@ -1,9 +0,0 @@
-//Was having issue in pm2 logs for random requests to xmlrpc.php, likely from bots trying to target WordPress servers
-//This code ensures that any request to /xmlrpc.php returns a 404 Not Found response.
-export const loader = async () => {
-  return new Response("Not Found", { status: 404 });
-};
-
-export const action = async () => {
-  return new Response("Not Found", { status: 404 });
-};
