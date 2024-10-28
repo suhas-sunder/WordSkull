a7961049d45d25d62273327801a23e23c95b126d
Suhas Sunder <suhas@live.ca>
2024-10-28 01:03:44 -0400

    feat: links
    -Add relevant links to projects, socials, and games to the links page.
    
    -Update title and add a description to links page.

--- app/routes/socials.tsx
+++ app/routes/socials.tsx
@@ -1,4 +1,5 @@
 import { MetaFunction } from "@remix-run/node";
+import { Link } from "react-router-dom";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,22 +15,216 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-function socials() {
+export default function Socials() {
   return (
-    <div className="flex flex-col">
-      <header>
-        <h1>Links</h1>
+    <div className="flex flex-col mt-3 sm:mt-5 justify-center items-center">
+      <header className="flex flex-col justify-center items-center text-center max-w-[1200px] mb-8 gap-5">
+        <h1 className="mt-[0.7em] text-5xl font-nunito text-skull-dark-brown">
+          WordSkull - Links
+        </h1>
+        <p className="font-lato text-xl  tracking-wider leading-loose">
+          Welcome! This page serves as my personal link tree, providing easy
+          access to my projects and social media. I hope you find it helpful!
+        </p>
       </header>
-      <main>
-        <div>
-          <h2>Socials</h2>
-        </div>
-        <div>
-          <h2>Related Projects</h2>
-        </div>
+      <main className="flex flex-col font-lato text-skull-dark-brown tracking-wider leading-loose w-full gap-4 max-w-[1200px]">
+        <h2 className="text-2xl font-nunito text-skull-super-dark-brown">
+          My Projects
+        </h2>
+
+        <ul>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.emojikitchen.com"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Emoji Kitchen Game
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.emojikitchen.com"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Emoji Kitchen Game
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.dragonmythology.com"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              DragonMythology
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.freetypingcamp.com"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Free Typing Camp
+            </Link>
+          </li>
+        </ul>
+        <h2 className="text-2xl font-nunito text-skull-super-dark-brown">
+          Socials
+        </h2>
+        <ul>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.linkedin.com/in/word-skull/"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              LinkedIn
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.youtube.com/@WordSkullGame"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              YouTube
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.facebook.com/profile.php?id=61566613301910"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Facebook
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://x.com/WordSkullGame"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Twitter
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://mastodon.social/@wordskull"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Mastadon
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.linkedin.com/in/word-skull/"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              LinkedIn
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.reddit.com/r/WordSkull/"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Reddit
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.tiktok.com/@wordskull"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              TikTok
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://github.com/suhas-sunder"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              GitHub
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.suhassunder.com/"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              Portfolio
+            </Link>
+          </li>
+        </ul>
+        <h2 className="text-2xl font-nunito text-skull-super-dark-brown">
+          Fun word games
+        </h2>
+        <ul>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.wordskull.com/word-skull-game-easy-mode"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              WordSkull (3 - 5 letters) Easy Mode - Defeat the boneheads!
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.wordskull.com/word-skull-game-medium-mode"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              WordSkull(3 - 6 letters) Medium Mode
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.wordskull.com/word-skull-game-hard-mode"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              WordSkull (3 - 7 letters) Hard Mode - Defeat the reapers!
+            </Link>
+          </li>
+          <li>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to="https://www.wordskull.com/word-skull-game-extreme-mode"
+              className="font-lora hover:text-skull-dark-brown text-skull-super-dark-brown"
+            >
+              WordSkull (3 - 9 letters) Extreme Mode - Defat the royal lichen!
+            </Link>
+          </li>
+        </ul>
       </main>
     </div>
   );
 }
-
-export default socials;
